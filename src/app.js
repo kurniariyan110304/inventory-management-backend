c95f2a4926d45c45658e353a8465fe8c2849264d
kurniariyan110304 <kurniariyan110304@gmail.com>
2024-10-04 16:07:31 +0700

    sistem autentikasi

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello There")
+})
+
+const authController = require("./auth/auth.controller");
+
+app.use("/api/auth", authController);
+
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT)
+})      
